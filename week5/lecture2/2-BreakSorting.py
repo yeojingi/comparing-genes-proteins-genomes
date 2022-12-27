bc88cf3094a6e15c12aefc35757d5ab0b4c0604c
yeojingi <businessjingi@gmail.com>
2022-12-27 17:03:08 +0900

    week5

---
+++ week5/lecture2/2-BreakSorting.py
@@ -0,0 +1,62 @@
+def two_break_distance(P, Q, maxSynteny):
+  path_p = [0] * (maxSynteny * 2 + 1)
+  path_q = [0] * (maxSynteny * 2 + 1)
+
+  for cycle in P:
+    for i in range(len(cycle)):
+      if cycle[i] > 0:
+        e = cycle[i]*2 - 1
+      else:
+        e = -cycle[i]*2
+      
+      if cycle[i-1] > 0:
+        s = cycle[i-1] * 2
+      else:
+        s = -cycle[i-1] * 2 - 1
+      
+      path_p[s] = e
+      path_p[e] = s
+  
+  for cycle in Q:
+    for i in range(len(cycle)):
+      if cycle[i] > 0:
+        e = cycle[i]*2 - 1
+      else:
+        e = -cycle[i]*2
+      
+      if cycle[i-1] > 0:
+        s = cycle[i-1] * 2
+      else:
+        s = -cycle[i-1] * 2 - 1
+      
+      path_q[s] = e
+      path_q[e] = s
+
+  dp = [0] * (maxSynteny * 2 + 1)
+  nCycle = 0
+  for i in range(1, len(path_p)):
+    cur = i
+    p = True
+    if dp[cur] == 0:
+      nCycle += 1
+
+    while dp[cur] == 0:
+      if p:
+        dp[cur] = 1
+        cur = path_p[cur]
+        p = False
+      else:
+        dp[cur] = 1
+        cur = path_q[cur]
+        p = True
+  
+  return maxSynteny - nCycle
+
+fn = "dataset_288_4.txt"
+f = open(f"./data/{fn}", "r")
+reads = f.readline().strip().lstrip('(').rstrip(')').split(')(')
+P = [ list(map(int, read.split())) for read in reads ]
+reads = f.readline().strip().lstrip('(').rstrip(')').split(')(')
+Q = [ list(map(int, read.split())) for read in reads ]
+res = two_break_distance(P, Q, max( [max([abs(n) for n in cycle]) for cycle in P] ))
+print(res)
---
+++ week5/lecture6/TwoBreakOnGenomeGraph.py
@@ -0,0 +1,28 @@
+def two_break_on_genome_graph(GenomeGraph, i1, i2, i3, i4):
+  result_graph = []
+  e1i = 0
+  e2i = 0
+
+  for i in range(len(GenomeGraph)):
+    ele = GenomeGraph[i]
+
+    if ele in [[i1, i2], [i2, i1]]:
+      e1i = i
+    elif ele in [[i3, i4], [i4, i3]]:
+      e2i = i
+  
+  GenomeGraph[e1i][ GenomeGraph[e1i].index(i1) ] = i4
+  GenomeGraph[e2i][ GenomeGraph[e2i].index(i4) ] = i1
+
+  return GenomeGraph
+  result_graph = [ ", ".join(list(map(str, ele))) for ele in GenomeGraph]
+  return "(" + "), (".join(result_graph) + ")"
+
+# fn = "1.txt"
+# fn = "dataset_8224_2.txt"
+# f = open(f"./data/{fn}", "r")
+# reads = f.readline().strip().lstrip('(').rstrip(')').split('), (')
+# GenomeGraph = [ list(map(int, read.split(', '))) for read in reads ]
+# [i1, i2, i3, i4] = list(map(int, f.readline().strip().split(', ')))
+# res = two_break_on_genome_graph(GenomeGraph, i1, i2, i3, i4)
+# print(res)
