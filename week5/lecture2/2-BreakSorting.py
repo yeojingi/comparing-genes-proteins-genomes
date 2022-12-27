8417598205fa4fb26edc19068a419e9be5605265
yeojingi <businessjingi@gmail.com>
2022-12-27 17:42:04 +0900

    ㅠㅠ

--- week5/lecture2/2-BreakSorting.py
+++ week5/lecture2/2-BreakSorting.py
@@ -1,62 +1,45 @@
-def two_break_distance(P, Q, maxSynteny):
-  path_p = [0] * (maxSynteny * 2 + 1)
-  path_q = [0] * (maxSynteny * 2 + 1)
+from lib.ColoredEdges import colored_edges
+from lib.TwoBreakOnGenome import two_break_on_genome
 
-  for cycle in P:
-    for i in range(len(cycle)):
-      if cycle[i] > 0:
-        e = cycle[i]*2 - 1
-      else:
-        e = -cycle[i]*2
-      
-      if cycle[i-1] > 0:
-        s = cycle[i-1] * 2
-      else:
-        s = -cycle[i-1] * 2 - 1
-      
-      path_p[s] = e
-      path_p[e] = s
-  
-  for cycle in Q:
-    for i in range(len(cycle)):
-      if cycle[i] > 0:
-        e = cycle[i]*2 - 1
-      else:
-        e = -cycle[i]*2
-      
-      if cycle[i-1] > 0:
-        s = cycle[i-1] * 2
-      else:
-        s = -cycle[i-1] * 2 - 1
-      
-      path_q[s] = e
-      path_q[e] = s
+def two_break_distance(P, Q):
+  edges_Q = colored_edges(Q)
+  ress = []
 
-  dp = [0] * (maxSynteny * 2 + 1)
-  nCycle = 0
-  for i in range(1, len(path_p)):
-    cur = i
-    p = True
-    if dp[cur] == 0:
-      nCycle += 1
+  for edge_Q in edges_Q:
+    print('res', P)
+    ress.append(P)
+    edges_P = colored_edges(P)
+    [s, e] = edge_Q
 
-    while dp[cur] == 0:
-      if p:
-        dp[cur] = 1
-        cur = path_p[cur]
-        p = False
-      else:
-        dp[cur] = 1
-        cur = path_q[cur]
-        p = True
-  
-  return maxSynteny - nCycle
+    for edge_P in edges_P:
+      if s in edge_P:
+        [i1, i2] = [s, edge_P[edge_P.index(s)-1]]
+      elif e in edge_P:
+        [i3, i4] = [e, edge_P[edge_P.index(e)-1]]
+        # [i2, i4] = edge_P
+    
+    P = two_break_on_genome(P, i1, i2, i3, i4)
+    print()
+    
+  ress.append(P)
+  return ress
 
-fn = "dataset_288_4.txt"
+fn = "1.txt"
 f = open(f"./data/{fn}", "r")
 reads = f.readline().strip().lstrip('(').rstrip(')').split(')(')
 P = [ list(map(int, read.split())) for read in reads ]
 reads = f.readline().strip().lstrip('(').rstrip(')').split(')(')
 Q = [ list(map(int, read.split())) for read in reads ]
-res = two_break_distance(P, Q, max( [max([abs(n) for n in cycle]) for cycle in P] ))
-print(res)
+ress = two_break_distance(P, Q)
+
+output = ""
+for res in ress:
+  output += "("
+  tmp = []
+  for e in res:
+    if e > 0:
+      tmp.append(f"+{e}")
+    else:
+      tmp.append(f"{e}")
+  output += " ".join(tmp) + ")"
+print(output)
---
+++ week5/lecture2/lib/GraphToGenome.py
@@ -0,0 +1,54 @@
+def graph_to_genome(GenomeGraph):
+  P = []
+
+  N = max([max(edge) for edge in GenomeGraph])
+
+  path = [0] * (N+1)
+
+  for edge in GenomeGraph:
+    path[edge[0]] = edge[1]
+    path[edge[1]] = edge[1]-1 if edge[1] % 2 == 0 else edge[1]+1
+  
+  dp = [0] * (N+1)
+
+  for edge in GenomeGraph:
+    i = edge[0]
+    if dp[i] == 1:
+      continue
+    
+    start = i
+    dp[i] = 1
+    cur = path[i]
+    cycle = []
+    while dp[cur] == 0:
+      cycle.append(cur)
+
+      dp[cur] = 1
+      cur = path[cur]
+    cycle.append(start)
+    print('e', GenomeGraph, edge)
+    print(cycle)
+    cycle = [cycle[-2], cycle[-1]] + cycle[:-2]
+
+    print(cycle)
+    P.append(cycle_to_chromosome(cycle))
+
+  return P
+
+def cycle_to_chromosome(cycle):
+  chromosome_arr = []
+
+  for i in range(0, len(cycle), 2):
+    if cycle[i] > cycle[i+1]:
+      chromosome_arr.append(-cycle[i] // 2)
+    else:
+      chromosome_arr.append(cycle[i+1] // 2)
+  
+  return chromosome_arr
+
+# fn = "4.txt"
+# f = open(f"./data/{fn}", "r")
+# reads = f.readline().strip().lstrip('(').rstrip(')').split('), (')
+# reads = [ list(map(int, read.split(', '))) for read in reads]
+# res = graph_to_genome(reads)
+# print(res)
